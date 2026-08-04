dc8c37b4bb210c63a5aa13ec374754f1e8c6500b
Doug-Colin <102709219+Doug-Colin@users.noreply.github.com>
2026-08-04 17:08:53 -0400

    Add constructor method to toggle haveRead property

--- main.js
+++ main.js
@@ -4,11 +4,14 @@ function Book(title, author, pages, haveRead) {
   if (!new.target) {
     throw new Error("You must use the 'new' operator to call the constructor");
   }
-  this.id = crypto.randomUUID()
+  this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.haveRead = haveRead;
+  this.toggleHaveRead = function () {
+    haveRead = !haveRead;
+  };
   this.info = function () {
     console.log(`${title} by ${author}, ${pages} pages, haveRead: ${haveRead}`);
   };
