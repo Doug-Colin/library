18b3f00ab45e49662982fc0a45613f0e8f6cb282
Doug-Colin <102709219+Doug-Colin@users.noreply.github.com>
2026-08-04 16:42:54 -0400

    Add Book id property that generates random id

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function Book(title, author, pages, haveRead) {
   if (!new.target) {
     throw new Error("You must use the 'new' operator to call the constructor");
   }
-
+  this.id = crypto.randomUUID()
   this.title = title;
   this.author = author;
   this.pages = pages;
