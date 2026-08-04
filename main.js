ddd3abf548fdaa7f97ee02f25966bd2fa8c83b10
Doug-Colin <102709219+Doug-Colin@users.noreply.github.com>
2026-08-04 16:38:21 -0400

    Write primary functions

---
+++ main.js
@@ -0,0 +1,20 @@
+let library = [];
+
+function Book(title, author, pages, haveRead) {
+  if (!new.target) {
+    throw new Error("You must use the 'new' operator to call the constructor");
+  }
+
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.haveRead = haveRead;
+  this.info = function () {
+    console.log(`${title} by ${author}, ${pages} pages, haveRead: ${haveRead}`);
+  };
+}
+
+function addBookToLibrary(title, author, pages, haveRead) {
+  const book = new Book(title, author, pages, haveRead);
+  library.push(book);
+}
