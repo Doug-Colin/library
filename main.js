6663e52ac74da6d7f8aeb3b1b7c02065c0b90067
Doug-Colin <102709219+Doug-Colin@users.noreply.github.com>
2026-08-16 13:18:09 -0400

    Add slider to preview book rendering by quantity

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { mockBookData } from "./data/mock-book-data.js";
 
-let library = [ ...mockBookData];
+let library = [...mockBookData];
 
 function Book(title, author, pages, haveRead) {
   if (!new.target) {
@@ -40,25 +40,38 @@ closeAddBookForm.addEventListener("click", () => {
   dialog.close();
 });
 
+// --- Slider for viewing how books cards render as they scale in quantity. ---
+
+const bookCountSlider = document.querySelector("#book-count-slider");
+bookCountSlider.max = mockBookData.length;
+
+const sliderOutput = document.querySelector("#book-count-output");
+
+bookCountSlider.addEventListener("input", (event) => {
+  const sliderValue = event.target.value;
+  const bookQuantity = Number(sliderValue);
+
+  sliderOutput.textContent = bookQuantity;
+
+  renderBooks(mockBookData.slice(0, bookQuantity));
+});
+
 // --- Render View.  ---------------
 const booksListContainer = document.getElementById("books-list-container");
 const template = document.getElementById("book-card-template");
 
+function renderBooks(booksArray) {
+  booksListContainer.replaceChildren();
 
-function render() {
-booksListContainer.replaceChildren()
+  booksArray.forEach((book) => {
+    const clone = template.content.cloneNode(true);
 
+    clone.querySelector(".book-title").textContent = book.title;
+    clone.querySelector(".book-author").textContent = book.author;
+    clone.querySelector(".book-length").textContent = `${book.pages} pages`;
 
-library.forEach((book) => {
-  const clone = template.content.cloneNode(true);
-
-  clone.querySelector(".book-title").textContent = book.title;
-  clone.querySelector(".book-author").textContent = book.author;
-  clone.querySelector(".book-length").textContent = `${book.pages} pages`;
-
-  booksListContainer.append(clone);
-});
-
+    booksListContainer.append(clone);
+  });
 }
 
-render()
+renderBooks(mockBookData);
